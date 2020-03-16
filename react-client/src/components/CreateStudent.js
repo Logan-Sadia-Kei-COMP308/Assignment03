e3b3a76f084bac99de505031708c329a7d92519e
Logan Junhwi Kim <logan.kim0212@gmail.com>
2020-03-15 21:36:59 -0400

    delete student function also deletes ref courses

--- react-client/src/components/CreateStudent.js
+++ react-client/src/components/CreateStudent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Spinner from "react-bootstrap/Spinner";
 import Jumbotron from "react-bootstrap/Jumbotron";
