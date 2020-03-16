12076b7432b40e97b05e531afae9575d1f829840
Logan Junhwi Kim <logan.kim0212@gmail.com>
2020-03-15 22:55:34 -0400

    design fixed

--- react-client/src/components/ListAllCourses.js
+++ react-client/src/components/ListAllCourses.js
@@ -88,7 +88,7 @@ function ListAllCourses(props) {
                 <th>Semester</th>
               </tr>
             </thead>
-            <tbody class="cursor tr">{displayAllCourseTable}</tbody>
+            <tbody class="tr">{displayAllCourseTable}</tbody>
           </table>
         </div>
       </div>
